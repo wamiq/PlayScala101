1c5891544396e9236bb7071bde9f7140548d8331
wamiq <wamiq.bashir@gmail.com>
2013-05-25 18:57:42 +0100

    added project files

---
+++ target/scala-2.10/src_managed/main/controllers/routes.java
@@ -0,0 +1,19 @@
+// @SOURCE:/Users/wamiq/Documents/workspace/PlayScala101/conf/routes
+// @HASH:f904f23acbfbb0ded1d9ae85b262187e14e562de
+// @DATE:Sat May 25 15:54:50 BST 2013
+
+package controllers;
+
+public class routes {
+public static final controllers.ReverseApplication Application = new controllers.ReverseApplication();
+public static final controllers.ReverseAssets Assets = new controllers.ReverseAssets();
+public static class javascript {
+public static final controllers.javascript.ReverseApplication Application = new controllers.javascript.ReverseApplication();
+public static final controllers.javascript.ReverseAssets Assets = new controllers.javascript.ReverseAssets();    
+}   
+public static class ref {
+public static final controllers.ref.ReverseApplication Application = new controllers.ref.ReverseApplication();
+public static final controllers.ref.ReverseAssets Assets = new controllers.ref.ReverseAssets();    
+} 
+}
+              
